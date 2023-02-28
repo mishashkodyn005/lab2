7bf38720aa443198c448032662cb32129fb3b778
mishashkodyn005 <126239651+mishashkodyn005@users.noreply.github.com>
2023-03-01 01:18:45 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+/*
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        double res = (double) a / b;
+        System.out.println(res + "\n" + a % b);
+    }
+
+} */
+
+
+
+
+
+/*
+ import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt(), result;
+        result = x / 10 + x % 10;
+        System.out.println(result);
+    }
+}
+
+ */
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        a += 0.5;
+        System.out.println((int) a);
+    }
+}
